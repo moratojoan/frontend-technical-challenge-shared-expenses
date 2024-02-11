4998d30459e59b799a6f44bb46a5130bd254b75a
moratojoan <moratojoan@gmail.com>
2024-02-11 19:36:57 +0100

    add initial data to the repositories

--- src/app/infraestructure/repositories/local-storage-member.repository.ts
+++ src/app/infraestructure/repositories/local-storage-member.repository.ts
@@ -2,12 +2,22 @@ import { Observable, of } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { Member } from '../../domain/models/member.model';
 import { MemberRepository } from '../../domain/repositories/member.repository';
+import { getAppInitialData } from '../../mocks/app-initial-data';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LocalStorageMemberRepository implements MemberRepository {
   private storageKey: string = 'members';
+
+  constructor() {
+    const members = this.getAllMembers();
+    const defaultMembers = getAppInitialData().members;
+    localStorage.setItem(
+      this.storageKey,
+      JSON.stringify(members.length > 0 ? members : defaultMembers)
+    );
+  }
 
   private getAllMembers(): Member[] {
     const members: Member[] = JSON.parse(
@@ -19,5 +29,17 @@ export class LocalStorageMemberRepository implements MemberRepository {
   getAll(): Observable<Member[]> {
     const transactions = this.getAllMembers();
     return of(transactions);
+  }
+
+  set(member: Omit<Member, 'id'>): Observable<Member> {
+    const members = this.getAllMembers();
+    const newMemberId = members.length + 1;
+    const newMember: Member = {
+      id: newMemberId,
+      ...member,
+    };
+    const newMembers: Member[] = [...members, newMember];
+    localStorage.setItem(this.storageKey, JSON.stringify(newMembers));
+    return of(newMember);
   }
 }
--- src/app/infraestructure/repositories/local-storage-transaction.repository.ts
+++ src/app/infraestructure/repositories/local-storage-transaction.repository.ts
@@ -2,6 +2,7 @@ import { Observable, of } from 'rxjs';
 import { Transaction } from '../../domain/models/transaction.model';
 import { TransactionRepository } from '../../domain/repositories/transaction.repository';
 import { Injectable } from '@angular/core';
+import { getAppInitialData } from '../../mocks/app-initial-data';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +11,18 @@ export class LocalStorageTransactionRepository
   implements TransactionRepository
 {
   storageKey: string = 'transactions';
+
+  constructor() {
+    const transactions = this.getAllTransactions();
+    const defaultTransactions = getAppInitialData().transactions;
+    localStorage.setItem(
+      this.storageKey,
+      JSON.stringify(
+        transactions.length > 0 ? transactions : defaultTransactions
+      )
+    );
+  }
+
   getAllTransactions(): Transaction[] {
     const transactions: Transaction[] = JSON.parse(
       localStorage.getItem(this.storageKey) ?? '[]'
