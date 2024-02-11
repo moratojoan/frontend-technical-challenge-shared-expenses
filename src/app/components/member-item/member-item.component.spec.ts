42042c08ff5fbc6c34933ae05e0bb7aa07011ac9
moratojoan <moratojoan@gmail.com>
2024-02-11 19:47:33 +0100

    test: member item balance classes

--- src/app/components/member-item/member-item.component.spec.ts
+++ src/app/components/member-item/member-item.component.spec.ts
@@ -29,8 +29,6 @@ describe('MemberItemComponent', () => {
 
     fixture = TestBed.createComponent(MemberItemComponent);
     component = fixture.componentInstance;
-    component.memberBalance = memberBalance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
@@ -38,16 +36,48 @@ describe('MemberItemComponent', () => {
   });
 
   it('should render: member name, totalTransactions & balance', () => {
-    const compiled = fixture.debugElement.nativeElement;
+    component.memberBalance = memberBalance;
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement as HTMLElement;
     const item = compiled.querySelector('li');
-    expect(item.textContent).toContain(memberBalance.member.name);
-    expect(item.textContent).toContain(memberBalance.totalTransactions.value);
-    expect(item.textContent).toContain(
+    expect(item?.textContent).toContain(memberBalance.member.name);
+    expect(item?.textContent).toContain(memberBalance.totalTransactions.value);
+    expect(item?.textContent).toContain(
       getCurrencySymbol(memberBalance.totalTransactions.currency, 'wide')
     );
-    expect(item.textContent).toContain(memberBalance.balance.value);
-    expect(item.textContent).toContain(
+    expect(item?.textContent).toContain(memberBalance.balance.value);
+    expect(item?.textContent).toContain(
       getCurrencySymbol(memberBalance.balance.currency, 'wide')
     );
+  });
+
+  it('should render the balance with a class named "positive" when the balance is positive', () => {
+    component.memberBalance = memberBalance;
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement as HTMLElement;
+    const balance = compiled.querySelector('.balance.positive');
+    expect(balance).toBeTruthy();
+  });
+
+  it('should render the balance with a class named "positive" when the balance is zero', () => {
+    component.memberBalance = {
+      ...memberBalance,
+      balance: { value: 0, currency: memberBalance.balance.currency },
+    };
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement as HTMLElement;
+    const balance = compiled.querySelector('.balance.positive');
+    expect(balance).toBeTruthy();
+  });
+
+  it('should render the balance with a class named "negative" when the balance is negative', () => {
+    component.memberBalance = {
+      ...memberBalance,
+      balance: { value: -10, currency: memberBalance.balance.currency },
+    };
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement as HTMLElement;
+    const balance = compiled.querySelector('.balance.negative');
+    expect(balance).toBeTruthy();
   });
 });
