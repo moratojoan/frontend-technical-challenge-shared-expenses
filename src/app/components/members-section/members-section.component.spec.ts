bd6dfbe16eb275046562d03abe52dfd3b1af68dd
moratojoan <moratojoan@gmail.com>
2024-02-11 17:09:58 +0100

    tests: set a new member

--- src/app/components/members-section/members-section.component.spec.ts
+++ src/app/components/members-section/members-section.component.spec.ts
@@ -8,11 +8,12 @@ import { of } from 'rxjs';
 describe('MembersSectionComponent', () => {
   let component: MembersSectionComponent;
   let fixture: ComponentFixture<MembersSectionComponent>;
+  let MemberRepositorySpy: jasmine.SpyObj<MemberRepository>;
 
   beforeEach(async () => {
-    const MemberRepositorySpy = jasmine.createSpyObj<MemberRepository>(
+    MemberRepositorySpy = jasmine.createSpyObj<MemberRepository>(
       'MemberRepository',
-      ['getAll']
+      ['getAll', 'set']
     );
     MemberRepositorySpy.getAll.and.returnValue(of(getAppInitialData().members));
 
@@ -50,5 +51,40 @@ describe('MembersSectionComponent', () => {
         getAppInitialData().members[index].name
       );
     });
+  });
+
+  it('should open a modal when click the button', async () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const button = compiled.querySelector('button') as HTMLButtonElement;
+    button?.click();
+    await fixture.whenStable();
+    const dialog = compiled.querySelector('dialog') as HTMLDialogElement;
+    expect(dialog.hasAttribute('open')).toBeTruthy();
+  });
+
+  it('should set a new member', async () => {
+    const newName = 'Texto de prueba';
+    MemberRepositorySpy.set.and.returnValue(of({ id: 4, name: newName }));
+
+    const compiled = fixture.nativeElement as HTMLElement;
+    const button = compiled.querySelector('button') as HTMLButtonElement;
+    button.click();
+    await fixture.whenStable();
+    const dialog = compiled.querySelector('dialog') as HTMLDialogElement;
+    const input = dialog.querySelector('input') as HTMLInputElement;
+    const submitButton = dialog.querySelector(
+      "button[type='submit']"
+    ) as HTMLButtonElement;
+
+    input.value = newName;
+    input.dispatchEvent(new Event('input'));
+    submitButton.click();
+    await fixture.whenStable();
+    fixture.detectChanges();
+
+    expect(dialog.hasAttribute('open')).toBeFalsy();
+    const item =
+      compiled.querySelectorAll('li')[getAppInitialData().members.length];
+    expect(item.textContent).toContain(newName);
   });
 });
