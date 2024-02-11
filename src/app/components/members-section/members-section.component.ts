b8adab8386e8f01806481da8dc38e43d0e33faad
moratojoan <moratojoan@gmail.com>
2024-02-11 13:26:15 +0100

    add dialog to members-section

--- src/app/components/members-section/members-section.component.ts
+++ src/app/components/members-section/members-section.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, inject } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  inject,
+} from '@angular/core';
 import { Member } from '../../domain/models/member.model';
 import { GetAllMembersUseCase } from '../../application/get-all-members-use-case';
 import { ButtonComponent } from '../ui/button/button.component';
@@ -14,11 +20,31 @@ export class MembersSectionComponent implements OnInit {
   members: Member[] = [];
   getAllMembers = inject(GetAllMembersUseCase);
 
+  @ViewChild('addMemberDialog', { static: true })
+  dialog!: ElementRef<HTMLDialogElement>;
+
   ngOnInit(): void {
     this.getAllMembers.execute().subscribe({
       next: (members) => {
         this.members = members;
       },
     });
+  }
+
+  showAddMemberDialog() {
+    this.dialog.nativeElement.showModal();
+  }
+
+  closeModal(e: MouseEvent) {
+    const dialog = this.dialog.nativeElement;
+    const dialogDimensions = dialog.getBoundingClientRect();
+    if (
+      e.clientX < dialogDimensions.left ||
+      e.clientX > dialogDimensions.right ||
+      e.clientY < dialogDimensions.top ||
+      e.clientY > dialogDimensions.bottom
+    ) {
+      dialog.close();
+    }
   }
 }
