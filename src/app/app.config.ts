3f6c204ea1fda91bfe412d305b809a9b384377dc
moratojoan <moratojoan@gmail.com>
2024-06-02 12:40:34 +0200

    remove unnecessary appConfig providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,39 +2,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { GetAllTransactionsUseCase } from './application/get-all-transactions-use-case';
-import { GetAllMembersUseCase } from './application/get-all-members-use-case';
-import { SetMemberUseCase } from './application/set-member-use-case';
 import { TransactionRepository } from './domain/repositories/transaction.repository';
 import { MemberRepository } from './domain/repositories/member.repository';
 import { LocalStorageTransactionRepository } from './infraestructure/repositories/local-storage-transaction.repository';
 import { LocalStorageMemberRepository } from './infraestructure/repositories/local-storage-member.repository';
-import { GetBalanceUseCase } from './application/get-balance-use-case';
-import { SetTransactionUseCase } from './application/set-transaction-use-case';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    {
-      provide: GetAllTransactionsUseCase,
-      useClass: GetAllTransactionsUseCase,
-    },
-    {
-      provide: GetAllMembersUseCase,
-      useClass: GetAllMembersUseCase,
-    },
-    {
-      provide: SetMemberUseCase,
-      useClass: SetMemberUseCase,
-    },
-    {
-      provide: SetTransactionUseCase,
-      useClass: SetTransactionUseCase,
-    },
-    {
-      provide: GetBalanceUseCase,
-      useClass: GetBalanceUseCase,
-    },
     {
       provide: TransactionRepository,
       useClass: LocalStorageTransactionRepository,
